b926cd2fdf1d8a8d0000816cae5189028197b702
Joppy Furr <joppyfurr@gmail.com>
2019-07-26 22:30:30 +1200

    Update build script to auto-detect t45 / t85 and auto-set fuses

--- build.sh
+++ build.sh
@@ -1,16 +1,44 @@
 #!/bin/sh
 
-# Furry Badge
+# Furry Badge build script.
+
+# Configuration
+TTY="/dev/ttyUSB0"
+
+# Auto-detect chip by providing the wrong part number and checking the error
+LINE="$(avrdude -c avr910 -P ${TTY} -p ATtiny2313 -q 2>&1 | grep 'Device signature')"
+if $(echo "${LINE}" | grep -q "probably t85")
+then
+    echo "Detected an ATtiny85"
+    CHIP="t85"
+    GCC_CHIP="attiny85"
+elif $(echo "${LINE}" | grep -q "probably t45")
+then
+    echo "Detected an ATtiny45"
+    CHIP="t45"
+    GCC_CHIP="attiny45"
+else
+    echo "Did not detect either a Tiny85 or Tiny45. Exiting."
+    exit 1
+fi
 
 # Compile
-avr-gcc -g -Os -Wall -mcall-prologues -mmcu=attiny85 *.c -o badge.obj || exit
+avr-gcc -g -Os -Wall -mcall-prologues -mmcu=${GCC_CHIP} *.c -o badge.obj || exit
 
 # Generate .hex
 avr-objcopy -R .eeprom -O ihex badge.obj badge.hex || exit
 
-# Set fuses
-# avrdude -p t85 -c avr910 -P /dev/ttyUSB0 -U lfuse:w:0xe2:m
+# Set fuses if needed
+LFUSE="$(avrdude -p ${CHIP} -c avr910 -P ${TTY} -U lfuse:r:-:h 2>&1 | grep '^0x')"
+if [ "${LFUSE}" = "0xe2" ]
+then
+    echo "L-fuse already set to 0xe2"
+else
+    echo "Writing L-fuse to 0xe2"
+    avrdude -p ${CHIP} -c avr910 -P ${TTY} -U lfuse:w:0xe2:m
+fi
 
 # Program
-avrdude -p t85 -c avr910 -P /dev/ttyUSB0 -U flash:w:badge.hex
+echo "Writing badge.hex to chip..."
+avrdude -p ${CHIP} -c avr910 -P ${TTY} -U flash:w:badge.hex
 
